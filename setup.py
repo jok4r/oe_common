31a99a78a72c4fd55debff36a4bc4f092ea98a82
jok4r <opensuse12@gmail.com>
2022-01-05 10:17:25 +0300

    Required python version now is 3.6

--- setup.py
+++ setup.py
@@ -19,13 +19,13 @@ with open('LICENSE.txt') as f:
 
 setup(
     name='oe_common',
-    version='1.2.0',
+    version='1.2.1',
     author='Dmitry Yakovlev',
     author_email='info@overhosting.ru',
     description='OeCommon',
     long_description=readme,
     long_description_content_type='text/markdown',
-    python_requires='>=3.7',
+    python_requires='>=3.6',
     packages=['oe_common'],
     licence='MIT',
     ext_modules=[get_directory_size],
